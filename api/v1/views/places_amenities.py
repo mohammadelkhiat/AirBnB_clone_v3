0ff0bc39d146900f00715c7cd3ae6cebf1afae01
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-02-27 03:11:29 +0200

    fix: View for the link between Place objects and Amenity objects.

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -12,7 +12,7 @@ from models import storage_t
 @app_views.route('/places/<place_id>/amenities', methods=['GET'])
 def places_amenities(place_id):
     """ Endpoint to handle http methods for request to /places/<place_id>/amenities"""
-    place = storage.get('Place', place_id)
+    place = storage.get(Place, place_id)
     if place is None:
         abort(404)
 
@@ -22,11 +22,11 @@ def places_amenities(place_id):
 @app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['POST', 'DELETE'])
 def places_amenities_with_id(place_id, amenity_id):
     """ Endpoint to handle http methods for request to /places/<place_id>/amenities/<amenity_id>"""
-    place = storage.get('Place', place_id)
+    place = storage.get(Place, place_id)
     if place is None:
         abort(404)
 
-    amenity = storage.get('Amenity', amenity_id)
+    amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
         abort(404)
 
